44c8354937c1d4978c085a62aef3349c6789e1f5
Horidas Roy <horidasr@gmail.com>
2017-05-16 23:56:54 +0600

    Getting Started with javascript

---
+++ Getting_started.js
@@ -0,0 +1,10 @@
+alert("Hello..This The first javascript code");
+if(confirm("Are u want to continue"))
+
+	document.location.href = "http://www.google.com";
+
+var user_name;
+
+user_name = prompt("What is your name ?");
+
+document.writeln("welcome to Google " + user_name + "!");
