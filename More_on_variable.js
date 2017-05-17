b6c2fbacfeec035bcccb3a4561ad48e660cccade
Horidas Roy <horidasr@gmail.com>
2017-05-17 23:33:23 +0600

    Handling local and global variable of javascript

---
+++ More_on_variable.js
@@ -0,0 +1,14 @@
+
+function  show() {
+	// body...
+	var money = 2;
+
+	alert("The amount of money on function is: " + money);
+}
+
+var money = 66;
+
+alert("The amount of money on main function " + money);
+show();
+
+alert("The amount of money on main function is " + money);
