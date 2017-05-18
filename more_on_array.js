da2dc28a4c24401be0a73eae2bd6b1db1e93de41
Horidas Roy <horidasr@gmail.com>
2017-05-18 22:43:10 +0600

    More on array control of javascript

---
+++ more_on_array.js
@@ -0,0 +1,36 @@
+
+var pets = ["dogs", "cats","cows","rabbit","cats", "hamster","pengein"];
+//pets.sort();
+
+//alert(pets);
+
+//pets.reverse();
+//alert(pets);
+
+//pets.sort().reverse();
+//alert(pets);
+
+//alert(pets.indexOf("dogs"));
+
+var result = pets.slice(1, 5);
+alert(result);
+
+
+var pos = [],  searchAt = 0;
+
+do{
+	foundAt = pets.indexOf("cats", searchAt);
+	
+
+	if (foundAt != -1) {
+		// statement
+
+		pos.push(foundAt);
+
+		searchAt = foundAt + 1;
+	}
+}while (foundAt != -1) 
+	// statement
+
+
+alert(pos);
