e8b08412c43cd6b71a376cb6d12816e895b8d2ff
Horidas Roy <horidasr@gmail.com>
2017-06-02 00:51:15 +0600

    More on event Handling

---
+++ more_on_event.js
@@ -0,0 +1,15 @@
+function good () {
+	// body... 
+
+	alert("I think right choice..");
+}
+
+function bad () {
+	// body... 
+	alert("I think bad choice ..");
+}
+
+function change_color (new_color) {
+	// body... 
+	document.getElementById("mydiv").style.background = new_color;
+}
