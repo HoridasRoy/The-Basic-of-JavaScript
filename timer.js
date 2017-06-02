d22cbc54e2ece5c1c0ad06bde68052b6e669114b
Horidas Roy <horidasr@gmail.com>
2017-06-02 23:38:20 +0600

    timer for image

--- timer.js
+++ timer.js
@@ -10,3 +10,14 @@ function wake_message () {
 
 	alert("wake up , wake up ,wake up...")
 }
+
+var the_image, x_position = 0,the_timer;
+function set_timer () {
+	// body... 
+	
+
+	the_image = document.getElementById("image");
+	x_position = x_position + 1;
+	the_image.style.left  = x_position;
+	the_timer = setTimeout(set_timer,50);
+}
