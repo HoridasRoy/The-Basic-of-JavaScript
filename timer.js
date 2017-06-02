e7bf2895fd4322e51282ed53375a20a73aaf660c
Horidas Roy <horidasr@gmail.com>
2017-06-02 23:24:10 +0600

    Timer

---
+++ timer.js
@@ -0,0 +1,12 @@
+function show_things () {
+	// body... 
+	var wait = prompt("How much time u want to sleep...");
+
+	setTimeout(wake_message, wait);
+}
+
+function wake_message () {
+	// body... 
+
+	alert("wake up , wake up ,wake up...")
+}
